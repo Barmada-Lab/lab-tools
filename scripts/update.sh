71a2994ed6210f21fa5b2a77e8f2860b1225129d
kerowak <boiwaq@gmail.com>
2022-11-03 11:14:38 -0400

    moved update script into scripts dir

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,19 +4,21 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 SCRIPT_FILE=`basename "$0"`
 SCRIPT_PATH=$SCRIPT_DIR/$SCRIPT_FILE
 
+REPO_BASE=$SCRIPT_DIR/..
+
 setup() {
-    cd $SCRIPT_DIR/notebooks
+    cd $REPO_BASE/notebooks
     poetry update
     module load R
     poetry run R -e "install.packages('IRkernel', repos='http://cran.us.r-project.org'); IRkernel::installspec()"
-    poetry run ipython kernel install --user --name barma 
+    poetry run ipython kernel install --name barma --user
     if [ ! -d  $HOME/.jupyter ]; then
         mkdir $HOME/.jupyter
     fi;
 
 # in   dentation; :-  ')
-cat >$HOME/.jupyter/jupyter_lab_config.py <<- EOM
-c.ServerApp.root_dir = "$SCRIPT_DIR/notebooks/notebooks"
+cat > $HOME/.jupyter/jupyter_lab_config.py <<- EOM
+c.ServerApp.root_dir = "$REPO_BASE/notebooks/notebooks"
 c.ServerApp.autoreload = True
 EOM
 
