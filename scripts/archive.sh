5894fc6d0cbfa4ee979108aaefb57c31d1e60e5b
kerowak <boiwaq@gmail.com>
2023-01-16 10:56:07 -0500

    fixed a weird bug
    despite setting "set -e", the removed "if" statement was still executed
    after the line beginning with "tar -czf" failed...

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -11,19 +11,19 @@ tar_scp_rm() {
     set -e
 
     dir=$1
+
     # clean ephemeral data
     rm -rf $dir/{analysis,processed_imgs}
+
     dirname=$(basename $dir)
     tar=$SCRATCH/${dirname}.tar.gz
-    # archive the experiment
-    tar -czf $tar $dir && scp $tar $DEST && rm -rf $tar $dir
+    cached=$LOCAL/$dirname
 
-    # remove any local cached copies if they exist
-    if [ -d $LOCAL/$dirname ]; then
-        rm -rf $LOCAL/$dirname
-    fi
+    # Archive the experiment
+    tar -czf $tar $dir && scp $tar $DEST && rm -rf $tar $dir $cached
+
 }
 
 export -f tar_scp_rm
 
-find $1 -mindepth 1 -maxdepth 1 -type d | xargs -I {} bash -c 'tar_scp_rm "{}"'
+find $1 -mindepth 1 -maxdepth 1 -type d -mtime +60 -exec bash -c 'tar_scp_rm "$0"' {} \;
