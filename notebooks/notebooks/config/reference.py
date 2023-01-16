d9bb097c61dc03f7e9f312dc055e53f834e1b8cd
kerowak <boiwaq@gmail.com>
2023-01-16 14:23:42 -0500

    added a 'process many' notebook

--- notebooks/notebooks/config/reference.py
+++ notebooks/notebooks/config/reference.py
@@ -2,8 +2,6 @@ from pathlib import Path
 import os
 
 SCRATCH_PATH = Path("/scratch/sbarmada_root/sbarmada0") / os.environ.get("USER") / ".lab_tools"
-if not SCRATCH_PATH.exists():
-    os.makedirs(SCRATCH_PATH, exist_ok=True)
 EXPERIMENT_DIRECTORY = Path("/nfs/turbo/umms-sbarmada/experiments")
 ILASTIK_BINARY = Path("/nfs/turbo/umms-sbarmada/shared/ilastik-1.4.0b27-Linux/run_ilastik.sh")
 ILASTIK_CLASSIFIERS = Path("/nfs/turbo/umms-sbarmada/shared/classifiers")
