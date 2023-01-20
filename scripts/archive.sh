d68743386a7ddfcb52226ab183118d9bd25272fd
Jacob Waksmacki <jwaksmac@umich.edu>
2023-01-20 17:07:25 -0500

    archive.sh logging and filename handling improved

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -12,19 +12,20 @@ tar_scp_rm() {
 
     dir=$1
 
+    echo "archiving $dir..."
     # clean ephemeral data
     rm -rf $dir/{analysis,processed_imgs}
 
     base=$dir/..
-    dirname=$(basename $dir)
+    dirname=$(basename "$dir")
     tar=$SCRATCH/${dirname}.tar.gz
     cached=$LOCAL/$dirname
 
     # Archive the experiment
-    tar -czf $tar -C $base $dirname && scp $tar $DEST && rm -rf $tar $dir $cached
+    tar -czf "$tar" -C "$base" "$dirname" && scp -q "$tar" "$DEST" && rm -rf "$tar" "$dir" "$cached"
 
 }
 
 export -f tar_scp_rm
 
-find $ORIGIN -mindepth 1 -maxdepth 1 -type d -mtime +60 | head -30 | xargs -n 1 -I {} bash -c 'tar_scp_rm "$0"' {}
+find $ORIGIN -mindepth 1 -maxdepth 1 -type d -mtime +60 | head -30 | xargs -d '\n' -n 1 -I {} bash -c 'tar_scp_rm "$0"' {}
