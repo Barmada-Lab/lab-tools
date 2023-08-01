537683d8ee93296bd1e62f52533ea5b29dedee77
kerowak <boiwaq@gmail.com>
2023-08-01 14:50:03 -0400

    Clarified CLI arg

--- survival/survival/cli.py
+++ survival/survival/cli.py
@@ -7,8 +7,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("experiment_path", type=Path)
     parser.add_argument("scratch_path", type=Path)
-    parser.add_argument("--no-gedi", help="[NOT YET FUNCTIONAL] don't use rfp gedi", default=True, action='store_false')
     parser.add_argument("--single-cell", help="[NOT YET FUNCTIONAL] use single cell tracking", default=False, action='store_true')
+    parser.add_argument("--no-gedi", help="don't use rfp gedi", default=True, action='store_false')
     parser.add_argument("--save-masks", help="save labelled stacks", default=False, action='store_true')
     parser.add_argument("--avg-reg", help="average registration across plate", default=False, action='store_true')
     parser.add_argument("--cpus", help="num cpus", type=int, default=1, action="store")
@@ -17,4 +17,5 @@ def parse_args():
 
 def run():
     args = parse_args()
-    core.process(args.experiment_path, args.scratch_path, args.save_masks, args.single_cell, args.no_gedi, args.avg_reg, args.cpus)
+    use_gedi = not args.no_gedi
+    core.process(args.experiment_path, args.scratch_path, args.save_masks, args.single_cell, use_gedi, args.avg_reg, args.cpus)
