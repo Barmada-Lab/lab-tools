3fc43a1f8ea11fadcce1d22f66cb504c61e509d2
Jacob Waksmacki <jwaksmac@umich.edu>
2024-06-28 15:22:26 -0400

    Refactored fiftyone ingest
    Using pandas dataframes as a common interface for uploading fo datasets
    seems prudent. Experiment loaders for which the constituent data exists
    as image files on disk can be rewritten to match the CQ1 exeperiment
    loader. Integrating with fo is then straightforward.
    
    Experiment loaders that rely on chunked array formats are trickier.
    While exporting a PNG view of the experiment is easy, accessing the
    underlying raw data for quantitative purposes is less so.  My current
    thinking is to export such experiments as a series of TIFFs to provide
    a consistent file-based interface for analyses that use fiftyone.

--- cytomancer/fiftyone/click.py
+++ cytomancer/fiftyone/click.py
@@ -1,7 +1,6 @@
 from pathlib import Path
-import os
+import shutil
 
-from tqdm import tqdm
 from dask.distributed import Client
 import fiftyone as fo
 import click
@@ -18,17 +17,16 @@ def launch_app() -> None:
 @click.argument("dataset_name")
 def delete_dataset(dataset_name: str) -> None:
     dataset = fo.load_dataset(dataset_name)
-    for sample in tqdm(dataset):
-        os.remove(sample.filepath)
-    dataset.delete()
+    shutil.rmtree(dataset.info["media_dir"])  # type: ignore
+    fo.delete_dataset(dataset_name)
 
 
 @click.command("ingest", help="Ingest a CQ1 dataset into FiftyOne. Other formats are not yet supported.")
 @experiment_dir_argument()
 def ingest(experiment_dir: Path) -> None:
     with Client(n_workers=8, threads_per_worker=2) as client:  # noqa: F841
-        from .ingest import ingest_cq1_longitudinal as ingest_fiftyone
-        ingest_fiftyone(experiment_dir)
+        from .ingest import ingest_cq1
+        ingest_cq1(experiment_dir)
 
 
 def register(cli: click.Group) -> None:
--- cytomancer/fiftyone/ingest.py
+++ cytomancer/fiftyone/ingest.py
@@ -1,43 +1,43 @@
-from more_itertools import chunked
 from pathlib import Path
 import logging
 import uuid
 
 from distributed import as_completed, get_client
-from fiftyone import ViewField as F
 from skimage import exposure  # type: ignore
 from tqdm import tqdm
 from PIL import Image
-import xarray as xr
 import fiftyone as fo
+import pandas as pd
 import tifffile
 import dask
 
-from cytomancer.experiment import Axes
 from cytomancer.config import config
 from cytomancer.io import cq1_loader
 
 logger = logging.getLogger(__name__)
 
 
-def ingest_cq1_longitudinal(experiment_path: Path):
+def get_or_create_dataset(name: str) -> fo.Dataset:
+    media_dir = config.fo_cache / name
+    media_dir.mkdir(parents=True, exist_ok=True)
 
-    client = get_client()
-
-    experiment_name = experiment_path.name
-    cache_dir = config.fo_cache / experiment_name
-    cache_dir.mkdir(parents=True, exist_ok=True)
-
-    if fo.dataset_exists(experiment_name):
+    if fo.dataset_exists(name):
         raise ValueError("Dataset already exists; delete before re-ingesting")
 
-    dataset = fo.Dataset(name=experiment_name)
+    dataset = fo.Dataset(name=name)
+    dataset.info["media_dir"] = str(media_dir)  # type: ignore
     dataset.persistent = True
+    return dataset
 
-    df, _, _ = cq1_loader.get_experiment_df(experiment_path, ordinal_time=True)
+
+def ingest_experiment_df(dataset: fo.Dataset, df: pd.DataFrame):
+
+    client = get_client()
     axes = df.index.names
+    media_dir = Path(dataset.info["media_dir"])  # type: ignore
 
-    def create_sample(coord_row):
+    @dask.delayed
+    def prepare_sample(coord_row):
         coords, row = coord_row
         path = row["path"]
         tags_dict = dict(zip(axes, coords))
@@ -45,67 +45,24 @@ def ingest_cq1_longitudinal(experiment_path: Path):
         arr = tifffile.imread(path)
         rescaled = exposure.rescale_intensity(arr, out_range="uint8")
         image = Image.fromarray(rescaled)
-        png_path = cache_dir / f"{uuid.uuid4()}.png"
+        png_path = media_dir / f"{uuid.uuid4()}.png"
         image.save(png_path, format="PNG")
 
         return (path, png_path, tags_dict)
 
-    chunks = list(chunked(df.iterrows(), 100))
-    for chunk in tqdm(chunks):
-        for _, result in as_completed(client.map(create_sample, chunk), with_results=True):
-            raw_path, png_path, tags = result  # type: ignore
-            sample = fo.Sample(filepath=png_path)
-            sample["raw_path"] = str(raw_path)  # attach the rawpath for quantitative stuff
-            for key, value in tags.items():
-                sample[key.name] = value
-            dataset.add_sample(sample)
+    samples = list(map(prepare_sample, df.iterrows()))
+    results = as_completed(client.compute(samples), with_results=True)
+    for _, (raw_path, png_path, tags) in tqdm(results, total=len(samples)):  # type: ignore
+        sample = fo.Sample(filepath=png_path)
+        sample["raw_path"] = str(raw_path)  # attach the rawpath for quantitative stuff
+        for key, value in tags.items():
+            sample[key.name] = value
+        dataset.add_sample(sample)
 
-    t1_view = (
-        dataset
-        .filter_field(Axes.TIME.name, F() == 0)
-        .filter_field(Axes.FIELD.name, F() == "1")
-    )
-    dataset.save_view("T1 go / no-go", t1_view, color="orange")
     dataset.save()
 
 
-def xarr_coords_to_tags(xarr):
-    pass
-
-
-def tags_to_xarr_coords(tags):
-    pass
-
-
-def ingest_experiment(experiment: xr.DataArray):
-
-    experiment_name = experiment.attrs["experiment_name"]
-    cache_dir = config.fo_cache / experiment_name
-    cache_dir.mkdir(parents=True, exist_ok=True)
-
-    if fo.dataset_exists(experiment_name):
-        raise ValueError("Dataset already exists; delete before re-ingesting")
-
-    dataset = fo.Dataset(name=experiment_name)
-    dataset.persistent = True
-
-    @dask.delayed
-    def create_sample(frame):
-        png_path = cache_dir / f"{uuid.uuid4()}.png"
-        image = Image.fromarray(frame)
-        image.save(png_path, format="PNG")
-        tags = xarr_coords_to_tags(frame.coords)
-        sample = fo.Sample(filepath=png_path, tags=tags)
-        dataset.add_sample(sample)
-
-    chunked = experiment.chunk({
-        Axes.TIME: 1,
-        Axes.REGION: 1,
-        Axes.FIELD: 1,
-        Axes.CHANNEL: 1,
-    })
-
-    assert chunked.chunks is not None
-
-    for idx in chunked.chunks:
-        print(idx)
+def ingest_cq1(base_path: Path):
+    dataset = get_or_create_dataset(base_path.name)
+    df, _, _ = cq1_loader.get_experiment_df(base_path)
+    ingest_experiment_df(dataset, df)
